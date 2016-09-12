6ae11031646243606ab41b3bbacd0814c1011165
Alejandro Pujol <alejandro.pujol@bybeeconcept.com>
2016-09-12 12:20:40 -0400

    Proyecto de Spring MVC

---
+++ spring-mvc/src/main/java/com/sirumatek/sandbox/springmvc/Application.java
@@ -0,0 +1,15 @@
+package com.sirumatek.sandbox.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application
+{
+
+    public static void main(String[] args) 
+    {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
