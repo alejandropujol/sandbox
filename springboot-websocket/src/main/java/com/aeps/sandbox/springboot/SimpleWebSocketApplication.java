f24e02bd0c2859284a7ee25cb56dc71d8b3108ff
Alejandro Pujol <alejandropujol@gmail.com>
2016-10-30 08:17:26 -0400

    Changes to deploy on Tomcat

--- springboot-websocket/src/main/java/com/aeps/sandbox/springboot/SimpleWebSocketApplication.java
+++ springboot-websocket/src/main/java/com/aeps/sandbox/springboot/SimpleWebSocketApplication.java
@@ -2,15 +2,22 @@ package com.aeps.sandbox.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Created by Alejandro on 10/28/2016.
  */
 @SpringBootApplication
-public class SimpleWebSocketApplication {
+public class SimpleWebSocketApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleWebSocketApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SimpleWebSocketApplication.class);
     }
 
 }
